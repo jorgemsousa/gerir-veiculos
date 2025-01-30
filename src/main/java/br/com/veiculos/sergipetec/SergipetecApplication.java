a22fc3b250c72bf49f7e8573b7e9bc1a978865e2
Jorge Sousa <jorgeferartsousa@gmail.com>
2025-01-30 12:25:43 -0300

    First commit: Criação do projeto.

---
+++ src/main/java/br/com/veiculos/sergipetec/SergipetecApplication.java
@@ -0,0 +1,13 @@
+package br.com.veiculos.sergipetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergipetecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergipetecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/veiculos/sergipetec/SergipetecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.veiculos.sergipetec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SergipetecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
