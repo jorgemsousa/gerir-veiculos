e4b9206caec93a15884d0e11c36e2fdd0cf59382
Jorge Sousa <jorgeferartsousa@gmail.com>
2025-02-02 15:23:43 -0300

    melhoria na conexão com o banco de dados aplicando camada de segurança criando .env

--- src/main/java/br/com/veiculos/sergipetec/config/DatabaseConfig.java
+++ src/main/java/br/com/veiculos/sergipetec/config/DatabaseConfig.java
@@ -1,13 +1,20 @@
 package br.com.veiculos.sergipetec.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5433/veiculos_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "ferart";
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = "jdbc:postgresql://"
+            + dotenv.get("DB_HOST") + ":"
+            + dotenv.get("DB_PORT") + "/"
+            + dotenv.get("DB_NAME");
+
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
